004df812746a5bb59e7548175aa15425e4a03584
Eduardo <educonts08@gmail.com>
2022-08-03 18:29:46 -0300

    project done

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -48,15 +48,42 @@ class NotesController{
   }
 
   async index(request, response){
-    const {title, user_id} = request.query
+    const {title, user_id, tags} = request.query
 
-    const notes = await knex("Movie_Notes")
-    .where({user_id})
-    .whereLike("title", `%${title}%`)
-    .orderBy("title")
+    let notes
+
+    if(tags){
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("Movie_Tags")
+      .select([
+        "Movie_Notes.id",
+        "Movie_Notes.title",
+        "Movie_Notes.user_id"
+      ])
+      .where("Movie_Notes.user_id", user_id)
+      .whereLike("Movie_Notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("Movie_Notes", "Movie_Notes.id", "Movie_Tags.note_id")
+      .orderBy("Movie_Notes.title")
+    }else{
+      notes = await knex("Movie_Notes")
+      .where({user_id})
+      .whereLike("title", `%${title}%`)
+      .orderBy("title")
+    }
+
+    const userTags = await knex("Movie_Tags").where({user_id})
+    const noteWithTags = notes.map(note =>{
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
 
 
-    return response.json(notes)
+    return response.json(noteWithTags)
   }
 }
 
