cd0857e3a0f51d83294554c278dd89ceb233ff25
Eduardo <educonts08@gmail.com>
2022-08-03 09:34:16 -0300

    create method updated

--- src/server.js
+++ src/server.js
@@ -5,12 +5,12 @@ const AppError = require('./utils/AppError')
 const express = require('express')
 
 const routes = require('./routes')
+migrationsRun()
 
 const app = express()
 app.use(express.json())
 
 app.use(routes)
-migrationsRun()
 
 app.use((error, request, response, next) => {
   if(error instanceof AppError){
