9edd842b490a1eafdfd1638cdd06366489e47b0b
Eduardo <educonts08@gmail.com>
2022-08-03 10:01:57 -0300

    created update method

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,13 +4,9 @@ const UsersController = require('../controllers/UsersController')
 
 const usersRoutes = Router()
 
-
-
-
-
-
 const userControllers = new UsersController()
 
 usersRoutes.post("/", userControllers.create)
+usersRoutes.put("/:id", userControllers.update)
 
 module.exports = usersRoutes
